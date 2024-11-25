b0eb5f690d3c662eb4cc125890a27fd87b4758bb
qwerdsa53 <qwerdsa53steam@gmail.com>
2024-11-25 22:25:58 +0300

    Initial commit

---
+++ src/main/java/qwerdsa53/trackmyfinance/TrackMyFinanceApplication.java
@@ -0,0 +1,13 @@
+package qwerdsa53.trackmyfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackMyFinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackMyFinanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/qwerdsa53/trackmyfinance/TrackMyFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package qwerdsa53.trackmyfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackMyFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
